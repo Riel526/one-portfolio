00358f96c89ab98d57622d1d20c266cfdd6fb5bc
Riel526 <gabpadolina05@gmail.com>
2024-02-22 03:36:10 +0800

    Updated resume
    Updated resume

--- src/components/SocialLinks.jsx
+++ src/components/SocialLinks.jsx
@@ -41,7 +41,7 @@ const SocialLinks = () => {
                 Resume <BsFillPersonLinesFill size ={30}/>
               </>
             ),
-            href:"/PADOLINA_Gabriel_Resume2023.pdf",
+            href:"/PADOLINA_Gabriel_Resume.pdf",
             style: 'rounded-tr-md',
             download: true
         }
